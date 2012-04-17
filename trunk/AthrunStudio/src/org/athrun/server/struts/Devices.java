33de5ee11d380f2612b0f3e681a7f184671887ac
taichan@taobao.com <taichan@taobao.com@d9bb56e2-10e4-4519-8022-6655a19093be>
2012-04-17 11:08:16 +0000

    move HelloWorld

---
+++ trunk/AthrunStudio/src/org/athrun/server/struts/Devices.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package org.athrun.server.struts;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+/**
+ * @author taichan
+ *
+ */
+public class Devices extends ActionSupport {
+
+	
+}
