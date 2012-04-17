0ae4d88efca258fec92c5971f3c395e4c51baf24
taobao_bingyang <taobao_bingyang@d9bb56e2-10e4-4519-8022-6655a19093be>
2012-04-17 02:59:08 +0000

    转移框架代码至新svn地址

---
+++ android/testcase_creator/src/org/athrun/android/framework/transform/action/ActionType.java
@@ -0,0 +1,17 @@
+package org.athrun.android.framework.transform.action;
+
+public enum ActionType {
+	GestureAction,
+	TouchAction,
+	TouchEventAction,
+	FlingAction,
+	
+	DeviceAction,
+	
+	ViewAction,
+	TextViewAction,
+	ViewGroupAction,
+	AbsListViewAction,
+	OptionItemAction,
+	TaobaoSkuViewAction
+}
