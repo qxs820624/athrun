3dbb554bfdc685942adebb543a042abbf0b4f732
ziyu.hch@taobao.com <ziyu.hch@taobao.com@d9bb56e2-10e4-4519-8022-6655a19093be>
2012-05-02 14:23:01 +0000

    10.6.8 与 10.7.3 instruments 命令路径变化

--- trunk/iOS/Athrun/AppTest.sh
+++ trunk/iOS/Athrun/AppTest.sh
@@ -40,6 +40,6 @@ $delCmd
 
 echo "all script run end!"
 
-#注意：次脚本在Mac 10.6.8版本运行通过。在Mac 10.7.3 版本系统中， instruments 命令相对 10.6.8版本有些变化，命令行运行instruments 采用如下命令：
+#注意：此脚本在Mac 10.6.8版本运行通过。在Mac 10.7.3 版本系统中， instruments 命令相对 10.6.8版本有些变化，命令行运行instruments 采用如下命令：
 #cmd="instruments -t /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/Library/Instruments/PlugIns/AutomationInstrument.bundle/Contents/Resources/Automation.tracetemplate $1 -e UIASCRIPT $file -e UIARESULTSPATH /Athrun/log/"
 
