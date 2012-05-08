065db89ce6ab53b6f7bc9d2ce9d7d9b45bd65d86
ziyu.hch@taobao.com <ziyu.hch@taobao.com@d9bb56e2-10e4-4519-8022-6655a19093be>
2012-05-08 02:58:27 +0000

    相关package 更名为 org.athrun.ios

---
+++ trunk/iOS/Athrun/Athrun_CS/Server/SocketServer/src/org/athrun/instruments/UIAActivityIndicator.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package org.athrun.instruments;
+
+/**
+ * @author ziyu.hch
+ *
+ */
+public class UIAActivityIndicator extends UIAElement {
+
+	/**
+	 * @param guid
+	 */
+	public UIAActivityIndicator(String guid) {
+		super(guid);
+		// TODO Auto-generated constructor stub
+	}
+
+}
