51fe97db69bdc43caa3c5684256a90d083957dc3
ziyu.hch@taobao.com <ziyu.hch@taobao.com@d9bb56e2-10e4-4519-8022-6655a19093be>
2012-05-08 02:55:02 +0000

    相关package 更名为 org.athrun.ios

---
+++ trunk/iOS/Athrun/Athrun_CS/Server/SocketServer/src/com/athrun/instruments/UIAStatusBar.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.athrun.instruments;
+
+/**
+ * @author ziyu.hch
+ * 
+ */
+public class UIAStatusBar extends UIAElement {
+
+	/**
+	 * @param guid
+	 */
+	public UIAStatusBar(String guid) {
+		super(guid);
+		// TODO Auto-generated constructor stub
+	}
+}
