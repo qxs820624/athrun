756a729050d422f58700efc3c0cebfff8e189dc0
ziyu.hch@taobao.com <ziyu.hch@taobao.com@d9bb56e2-10e4-4519-8022-6655a19093be>
2012-05-10 06:35:09 +0000

    add c/s runner file

---
+++ trunk/iOS/Athrun/CSRunner.js
@@ -0,0 +1,91 @@
+/**
+ * 用例名称: C/S模式运行，启动脚本
+ * 作   者: ziyu
+ * 日   期: 2012-05-08
+ * 备   注: Athrun C/S模式运行，启动脚本
+ *
+ */ 
+
+#import "./LibJS/athrunImports.js"
+
+target = UIATarget.localTarget();
+host = target.host();	
+
+UIALogger.logStart("The case is running.");
+
+try {
+	
+	var step = 1;
+	var isEnd = false;
+	while(!isEnd){
+		
+		var result = host.performTaskWithPathArgumentsTimeout("/Athrun/TcpSocket.sh",["null"],60);	
+		var stdout = result.stdout.split("##");
+		var type = stdout[0];
+		var script =stdout[1];
+		var sendToServer ="null";
+			
+		// UIALogger.logMessage("type:" + type);
+		
+		switch(type)
+		{
+			case "null":
+				break;
+			case "stringType":
+			case "booleanType":
+			case "voidType":
+				sendToServer = new Function("return "+ script)();
+				if(sendToServer ==null)
+					sendToServer = "";
+				break;
+			case "JSONArray":
+				var elementArray = new Array();
+				var elements = eval(script);
+				for(var i=0; i<elements.length;i++)
+				{
+					var element = {};
+					element.label = elements[i].label();
+					element.name = elements[i].name();
+					element.value = elements[i].value();
+					element.rect  = elements[i].rect();
+					element.guid = script +"[" + i + "]";
+					elementArray.push(element);
+				}
+				sendToServer = elementArray.toJSONString();
+				break;
+			case "JSONObject":
+				var element = eval(script);
+				var e = {};
+				e.label = element.label();
+				e.name = element.name();
+				e.value = element.value();
+				e.rect  = element.rect();
+				e.guid = script;
+				sendToServer = e.toJSONString();
+				break;
+			default:
+				sendToServer ="null";
+				isEnd =true;
+		}
+		if(sendToServer != "null")
+		{
+			UIALogger.logMessage("sendToServer:" + sendToServer);
+			host.performTaskWithPathArgumentsTimeout("/Athrun/TcpSocket.sh",[sendToServer],60);
+		}
+	}
+	
+	UIALogger.logPass("The case was passed.");
+
+} catch (e) {
+		
+	sendToServer = "Error #" + e;
+	
+	//if has exception ,send the exception to client.
+	host.performTaskWithPathArgumentsTimeout("/Athrun/TcpSocket.sh",[sendToServer],60);
+	//end Exit
+	host.performTaskWithPathArgumentsTimeout("/Athrun/TcpSocket.sh",[sendToServer],60);
+	UIALogger.logError(sendToServer);
+	UIALogger.logFail("The case was failed.");	
+	
+}
+
