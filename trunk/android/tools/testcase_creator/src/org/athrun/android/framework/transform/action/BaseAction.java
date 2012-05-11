f66341418ef2b79cda0f27f990554c508cce719d
taobao_bingyang <taobao_bingyang@d9bb56e2-10e4-4519-8022-6655a19093be>
2012-05-11 09:03:22 +0000

    修改等待时间

--- trunk/android/tools/testcase_creator/src/org/athrun/android/framework/transform/action/BaseAction.java
+++ trunk/android/tools/testcase_creator/src/org/athrun/android/framework/transform/action/BaseAction.java
@@ -9,17 +9,21 @@ import org.eclipse.jdt.core.dom.AST;
 import org.eclipse.jdt.core.dom.Block;
 import org.eclipse.jdt.core.dom.MethodInvocation;
 import org.eclipse.jdt.core.dom.Name;
+import org.eclipse.jdt.core.dom.NumberLiteral;
 import org.eclipse.jdt.core.dom.StringLiteral;
 
 public abstract class BaseAction implements IAction {
 	protected Logger logger = Logger.getLogger(getClass());
-	
+
 	private static final String COMMENT = "commentUsedForAthrun";
 	private static final String BLANK = "blankUsedForAthrun";
-	
+
 	private static final String SLEEP = "sleep";
 
 	public static final String ACTION_TYPE = "actiontype";
+	private static final String DELTA_TIME = "delta-time";
+
+	private static final int DEFAULT_MAX_FIND_VIEW_TIME = 5000;
 
 	protected String actiontype;
 	protected String activity;
@@ -34,7 +38,7 @@ public abstract class BaseAction implements IAction {
 	}
 
 	public abstract void toJavaCode(Block methodBlock);
-	
+
 	@SuppressWarnings("unchecked")
 	protected void createSpecial(Block methodBlock, String methodName) {
 		MethodInvocation methodInvocation = this.ast.newMethodInvocation();
@@ -50,26 +54,28 @@ public abstract class BaseAction implements IAction {
 	protected void createComment(Block methodBlock) {
 		createSpecial(methodBlock, COMMENT);
 	}
-	
+
 	@SuppressWarnings("unchecked")
 	protected void createBlank(Block methodBlock) {
 		methodBlock.statements().add(
-				this.ast.newExpressionStatement(getStatementForSpecial(methodBlock, BLANK)));
+				this.ast.newExpressionStatement(getStatementForSpecial(
+						methodBlock, BLANK)));
 	}
-	
-	protected MethodInvocation getStatementForSpecial(Block methodBlock, String methodName) {
+
+	protected MethodInvocation getStatementForSpecial(Block methodBlock,
+			String methodName) {
 		MethodInvocation methodInvocation = this.ast.newMethodInvocation();
 		methodInvocation.setName(ast.newSimpleName(methodName));
-		
+
 		return methodInvocation;
 	}
 
 	@SuppressWarnings("unchecked")
-	protected void createSleep(Block methodBlock) {
+	protected void createSleep(Block methodBlock, int waitTime) {
 		MethodInvocation methodInvocation = this.ast.newMethodInvocation();
 		methodInvocation.setExpression(ast.newName("Thread"));
 		methodInvocation.setName(ast.newSimpleName(SLEEP));
-		Name time = ast.newName(getSimpleNames("IViewElement.ANR_TIME"));
+		NumberLiteral time = ast.newNumberLiteral(String.valueOf(waitTime));
 		methodInvocation.arguments().add(time);
 		methodBlock.statements().add(
 				ast.newExpressionStatement(methodInvocation));
@@ -86,5 +92,16 @@ public abstract class BaseAction implements IAction {
 				list.add(name);
 		}
 		return (String[]) list.toArray(new String[list.size()]);
+	}
+
+	protected void createWaitIfNeeded(Block methodBlock) {
+		if (action.containsKey(DELTA_TIME)) {
+			String timeString = action.get(DELTA_TIME).split(":")[1];
+			int time = Integer.parseInt(timeString);
+			
+			if (time > DEFAULT_MAX_FIND_VIEW_TIME) {
+				createSleep(methodBlock, (time / 1000 + 2) * 1000);
+			}
+		}
 	}
 }
