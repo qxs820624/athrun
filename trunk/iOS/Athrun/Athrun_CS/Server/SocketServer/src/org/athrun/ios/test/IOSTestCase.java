4352b324bce352c5bda91f045ce9b7fd12417520
ziyu.hch@taobao.com <ziyu.hch@taobao.com@d9bb56e2-10e4-4519-8022-6655a19093be>
2012-05-11 06:02:57 +0000

    将 被测试应用的路径、用例运行方式 作为属性进行设置

--- trunk/iOS/Athrun/Athrun_CS/Server/SocketServer/src/org/athrun/ios/test/IOSTestCase.java
+++ trunk/iOS/Athrun/Athrun_CS/Server/SocketServer/src/org/athrun/ios/test/IOSTestCase.java
@@ -7,14 +7,38 @@ import org.athrun.ios.instruments.RunType;
 
 public class IOSTestCase extends TestCase {
 
+	private String appPath = "/Users/jerryding/taobao4iphone/build/Distribution-iphonesimulator/taobao4iphone.app";
+	private Boolean isDebug = false;
+
+	public String getAppPath() {
+		return appPath;
+	}
+
+	/**
+	 * set the test app's file path.
+	 * 
+	 * @param appPath
+	 */
+	public void setAppPath(String appPath) {
+		this.appPath = appPath;
+	}
+
+	public Boolean getIsDebug() {
+		return isDebug;
+	}
+
+	public void setIsDebug(Boolean isDebug) {
+		this.isDebug = isDebug;
+	}
+
 	@Override
 	protected void setUp() throws Exception {
 		// TODO Auto-generated method stub
 		super.setUp();
-		RunType.DEBUG = false;
+		RunType.DEBUG = this.isDebug;
 
 		Runtime.getRuntime().exec(
-				"/bin/bash /Athrun/RunScript.sh /Athrun/CSRunner.js", null);
+				"/bin/bash /Athrun/RunScript.sh " + this.appPath, null);
 	}
 
 	@Override
