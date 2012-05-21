8d460c9cd8eab3eeb325faa7b622126f43b92a94
ziyu.hch <ziyu.hch@taobao.com>
2012-05-21 06:22:49 +0000

    add findElements methods

--- iOS/Athrun/Athrun_CS/Server/SocketServer/src/org/athrun/ios/instruments/UIATarget.java
+++ iOS/Athrun/Athrun_CS/Server/SocketServer/src/org/athrun/ios/instruments/UIATarget.java
@@ -28,4 +28,24 @@ public class UIATarget {
 	public void delay(int second) throws Exception {
 		MySocket.getVoid(this.guid + ".delay(" + second + ")");
 	}
+
+	/**
+	 * 树形结构打印出当前元素节点下的所有子元素
+	 * 
+	 * @throws Exception
+	 *             C/S Socket 通讯异常
+	 * 
+	 */
+	public void printElementTree() throws Exception {
+
+		String elementTree = MySocket.getText("printElementTree('" + this.guid
+				+ "')");
+
+		System.out
+				.println("-----------------------------------------------------");
+		System.out.println("LogElementTree:");
+		System.out.println(elementTree.replaceAll("###", "\n"));
+		System.out
+				.println("-----------------------------------------------------");
+	}
 }
