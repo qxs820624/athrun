a3e9aeee65fa0d12269ec9df4c4783913c0da812
ziyu.hch@taobao.com <ziyu.hch@taobao.com@d9bb56e2-10e4-4519-8022-6655a19093be>
2012-05-22 05:43:28 +0000

    更改包名称，findElementByText

---
+++ trunk/iOS/Athrun/Athrun_CS/Server/SocketServer/src/org/athrun/ios/instrumentdriver/UIAApplication.java
@@ -0,0 +1,64 @@
+/**
+ * 
+ */
+package org.athrun.ios.instrumentdriver;
+
+/**
+ * @author ziyu.hch
+ * 
+ */
+public class UIAApplication {
+
+	private String guid = null;
+
+	public UIAApplication(String guid) {
+		// TODO Auto-generated constructor stub
+		this.guid = guid;
+	}
+
+	public UIAWindow[] windows() throws Exception {
+
+		return UIAElementHelp.windowArray(this.guid + ".windows()");
+	}
+
+	/**
+	 * mainWindow
+	 */
+	public UIAWindow mainWindow() throws Exception {
+
+		return UIAElementHelp.getWindow(this.guid + ".mainWindow()");
+	}
+
+	public UIAActionSheet actionSheet() throws Exception {
+
+		return UIAElementHelp.getActionSheet(this.guid + ".actionSheet()");
+	}
+
+	public UIAKeyboard keyboard() throws Exception {
+
+		return UIAElementHelp.getKeyboard(this.guid + ".keyboard()");
+	}
+
+	public UIANavigationBar navigationBar() throws Exception {
+
+		return UIAElementHelp.getNavigationBar(this.guid + ".navigationBar()");
+	}
+
+	public UIAStatusBar statusBar() throws Exception {
+
+		return UIAElementHelp.getStatusBar(this.guid + ".statusBar()");
+	}
+
+	/**
+	 * tabBar
+	 */
+	public UIATabBar tabBar() throws Exception {
+
+		return UIAElementHelp.getTabBar(this.guid + ".tabBar()");
+	}
+
+	public UIAToolbar toolbar() throws Exception {
+
+		return UIAElementHelp.getToolbar(this.guid + ".toolbar()");
+	}
+}
