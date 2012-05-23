e7885bd15ea3a0fc31e12a9b5511326bfadf6fc3
ziyu.hch@taobao.com <ziyu.hch@taobao.com@d9bb56e2-10e4-4519-8022-6655a19093be>
2012-05-23 08:39:21 +0000

    add InstrumentDriver project

---
+++ trunk/iOS/InstrumentDriver/src/org/athrun/ios/instrumentdriver/test/InstrumentDriverTestCase.java
@@ -0,0 +1,56 @@
+package org.athrun.ios.instrumentdriver.test;
+
+import junit.framework.TestCase;
+
+import org.athrun.ios.instrumentdriver.MySocket;
+import org.athrun.ios.instrumentdriver.RunType;
+
+public class InstrumentDriverTestCase extends TestCase {
+
+	private String appPath = "/Users/jerryding/taobao4iphone/build/Distribution-iphonesimulator/taobao4iphone.app";
+	private Boolean isDebug = true;
+
+	public String getAppPath() {
+		return appPath;
+	}
+
+	/**
+	 * set the test app's file path.
+	 * 
+	 * @param appPath
+	 */
+	public void setAppPath(String appPath) {
+		this.appPath = appPath;
+	}
+
+	public Boolean getIsDebug() {
+		return isDebug;
+	}
+
+	public void setIsDebug(Boolean isDebug) {
+		this.isDebug = isDebug;
+	}
+
+	@Override
+	protected void setUp() throws Exception {
+		// TODO Auto-generated method stub
+		super.setUp();
+		RunType.DEBUG = this.isDebug;
+
+		String cmd = String
+				.format("/bin/bash instruments -t /Developer/Platforms/iPhoneOS.platform/Developer/Library/Instruments/PlugIns/AutomationInstrument.bundle/Contents/Resources/Automation.tracetemplate  %s -e UIASCRIPT ./JSLib/CSRunner.js -e UIARESULTSPATH /Athrun/log/",
+						this.appPath);
+		// Runtime.getRuntime().exec("/bin/bash /Athrun/RunScript.sh " +
+		// this.appPath, null);
+		
+		Runtime.getRuntime().exec(cmd, null);
+	}
+
+	@Override
+	protected void tearDown() throws Exception {
+		// TODO Auto-generated method stub
+		super.tearDown();
+		MySocket.sendExit();
+		Runtime.getRuntime().exec("/bin/bash rm -rf *.trace ", null);
+	}
+}
