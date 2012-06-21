18e2571bb27a1717b74504f533b2fa73bf097ad8
taoziyu <taoziyu@d9bb56e2-10e4-4519-8022-6655a19093be>
2012-06-21 08:27:58 +0000

    move config file to src

--- trunk/iOS/InstrumentDriver/src/org/athrun/ios/instrumentdriver/test/InstrumentDriverTestCase.java
+++ trunk/iOS/InstrumentDriver/src/org/athrun/ios/instrumentdriver/test/InstrumentDriverTestCase.java
@@ -5,7 +5,6 @@ import org.athrun.ios.instrumentdriver.RunType;
 import org.athrun.ios.instrumentdriver.UIAApplication;
 import org.athrun.ios.instrumentdriver.UIATarget;
 import org.athrun.ios.instrumentdriver.UIAWindow;
-import org.athrun.ios.instrumentdriver.config.Config;
 import org.athrun.ios.instrumentdriver.config.DriverUtil;
 import org.junit.After;
 import org.junit.Before;
@@ -20,16 +19,13 @@ public class InstrumentDriverTestCase {
 	public UIAApplication app;
 	public UIAWindow win;
 
-	// private String appPath
-	// ="/Users/jerryding/taobao4iphone/build/Distribution-iphonesimulator/taobao4iphone.app";
+
 	private String appPath = DriverUtil.getApp();
-	// private String appPath
-	// ="/Users/athrun/Desktop/ios_cs/PNTB/build/Debug-iphonesimulator/PNTB.app";
+
 	private Boolean isDebug = DriverUtil.isDebug();
 
 	@Before
 	public void setUp() throws Exception {
-		// TODO Auto-generated method stub
 
 		this.target = UIATarget.localTarget();
 		this.app = target.frontMostApp();
@@ -44,7 +40,6 @@ public class InstrumentDriverTestCase {
 
 	@After
 	public void tearDown() throws Exception {
-		// TODO Auto-generated method stub
 
 		MySocket.sendExit();
 		String[] cmd = { "/bin/sh", "-c", "rm -rf *.trace " };
