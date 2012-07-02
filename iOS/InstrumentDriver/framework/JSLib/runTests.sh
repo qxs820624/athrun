165983e51eea836342fb57ec90801b9dd848ba6d
taoziyu <taoziyu@d9bb56e2-10e4-4519-8022-6655a19093be>
2012-07-02 15:36:19 +0000

    use single framework project

--- iOS/InstrumentDriver/framework/JSLib/runTests.sh
+++ iOS/InstrumentDriver/framework/JSLib/runTests.sh
@@ -1,10 +1,12 @@
 #! /bin/sh
 
 #@author mijun@douban.com
+#@author taichan@taobao.com
 
 XCODE_PATH=`xcode-select -print-path`
 TRACETEMPLATE="$XCODE_PATH/Platforms/iPhoneOS.platform/Developer/Library/Instruments/PlugIns/AutomationInstrument.bundle/Contents/Resources/Automation.tracetemplate"
 APP_LOCATION=$1
+INSTRUMENT_ROOT=$2
 
 #APP_LOCATION="/Users/komejun/Library/Application Support/iPhone Simulator/5.0/Applications/1622F505-8C07-47E0-B0F0-3A125A88B329/Recipes.app/"
 
@@ -18,6 +20,6 @@ echo $XCODE_PATH
 instruments \
 -t $TRACETEMPLATE \
 "$APP_LOCATION" \
--e UIASCRIPT  ./JSLib/CSRunner.js \
--e UIARESULTSPATH  ./JSLib/log/
+-e UIASCRIPT  "$INSTRUMENT_ROOT"/CSRunner.js \
+-e UIARESULTSPATH  "$INSTRUMENT_ROOT"/log/
 
