0d761e3b283ed88a5d69f93e6f6b130d2b073ad7
taoziyu <taoziyu@d9bb56e2-10e4-4519-8022-6655a19093be>
2012-07-20 05:09:52 +0000

    add segmentedControl type

---
+++ trunk/iOS/InstrumentDriver/framework/src/org/athrun/ios/instrumentdriver/UIASegmentedControl.java
@@ -0,0 +1,19 @@
+package org.athrun.ios.instrumentdriver;
+
+public class UIASegmentedControl extends UIAElement {
+
+	public UIASegmentedControl() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public UIASegmentedControl(String guid) {
+		super(guid);
+
+	}
+	
+	public UIAButton selectedButton() throws Exception {
+
+		return UIAElementHelp.getButton(this.guid + ".selectedButton()");
+	}
+
+}
