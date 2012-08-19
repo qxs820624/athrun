9074d99b9674d38c7934f68c07bb1f94fa6a09a1
taoziyu <taoziyu@d9bb56e2-10e4-4519-8022-6655a19093be>
2012-08-19 08:09:34 +0000

    添加真机配置支持，客户端异常的超时处理

--- iOS/InstrumentDriver/framework/JSLib/runTests.sh
+++ iOS/InstrumentDriver/framework/JSLib/runTests.sh
@@ -5,23 +5,24 @@
 
 XCODE_PATH=`xcode-select -print-path`
 TRACETEMPLATE="$XCODE_PATH/Platforms/iPhoneOS.platform/Developer/Library/Instruments/PlugIns/AutomationInstrument.bundle/Contents/Resources/Automation.tracetemplate"
+
 APP_LOCATION=$1
 INSTRUMENT_ROOT=$2
+isSimulator=$3
+UDID=$4
 
 #APP_LOCATION="/Users/komejun/Library/Application Support/iPhone Simulator/5.0/Applications/1622F505-8C07-47E0-B0F0-3A125A88B329/Recipes.app/"
-
-
 #APP_LOCATION="/Users/athrun/Desktop/TaoTest/build/Debug-iphonesimulator/TaoTest.app"
 
 echo $XCODE_PATH
-echo instruments -t $TRACETEMPLATE "$APP_LOCATION" -e UIASCRIPT  "$INSTRUMENT_ROOT"/CSRunner.js -e UIARESULTSPATH  "$INSTRUMENT_ROOT"/log/
 
-
-instruments \
--t $TRACETEMPLATE \
-"$APP_LOCATION" \
--e UIASCRIPT  "$INSTRUMENT_ROOT"/CSRunner.js \
--e UIARESULTSPATH  "$INSTRUMENT_ROOT"/log/
+if [ "$isSimulator" = "true" ] ; then
+  echo instruments -t $TRACETEMPLATE -D instrumentsDriver "$APP_LOCATION" -e UIASCRIPT  "$INSTRUMENT_ROOT"/CSRunner.js -e UIARESULTSPATH  "$INSTRUMENT_ROOT"/log/ -v
+  instruments -t $TRACETEMPLATE -D instrumentsDriver "$APP_LOCATION" -e UIASCRIPT  "$INSTRUMENT_ROOT"/CSRunner.js -e UIARESULTSPATH  "$INSTRUMENT_ROOT"/log/ -v
+else
+  echo instruments -w $UDID -t $TRACETEMPLATE -D instrumentsDriver "$APP_LOCATION" -e UIASCRIPT  "$INSTRUMENT_ROOT"/CSRunner.js -e UIARESULTSPATH  "$INSTRUMENT_ROOT"/log/ -v
+  instruments -w $UDID -t $TRACETEMPLATE -D instrumentsDriver "$APP_LOCATION" -e UIASCRIPT  "$INSTRUMENT_ROOT"/CSRunner.js -e UIARESULTSPATH  "$INSTRUMENT_ROOT"/log/ -v	
+fi
 
 
 
--- iOS/InstrumentDriver/framework/src/org/athrun/ios/instrumentdriver/MySocket.java
+++ iOS/InstrumentDriver/framework/src/org/athrun/ios/instrumentdriver/MySocket.java
@@ -5,6 +5,12 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 /**
  * @author ziyu.hch
@@ -19,12 +25,6 @@ public class MySocket {
 
 	public static ServerSocket server = null;
 
-	/*
-	 * public static String getGuid(String script) {
-	 * 
-	 * return send(ReturnedType.guidType + "##" + script); }
-	 */
-
 	public static void sendExit() throws Exception {
 
 		exit(ReturnedType.exitType.toString());
@@ -38,7 +38,26 @@ public class MySocket {
 		String request = null;
 		try {
 			Socket socket = null;
-			socket = server.accept();
+
+			final ExecutorService exec = Executors.newFixedThreadPool(1);
+			Callable<Socket> call = new Callable<Socket>() {
+				@Override
+				public Socket call() throws Exception {
+					return server.accept();
+				}
+			};
+			try {
+				Future<Socket> future = exec.submit(call);
+				// 超时时间 10s
+				socket = future.get(10 * 1000, TimeUnit.MILLISECONDS);
+			} catch (TimeoutException ex) {
+				String errorMsg = "被测应用无返回，socket 超时。";
+				System.err.println(errorMsg);
+				ex.printStackTrace();
+				exec.shutdown();
+				throw new Exception(errorMsg + ex);
+			}
+			exec.shutdown();
 
 			BufferedReader is = new BufferedReader(new InputStreamReader(
 					socket.getInputStream()));
@@ -51,7 +70,6 @@ public class MySocket {
 
 			os.print(exitMark);
 			os.flush();
-
 			// 刷新输出流，使Client马上收到该字符串
 
 			os.close();
@@ -63,14 +81,6 @@ public class MySocket {
 			System.out.println("Exception:" + e);
 		}
 	}
-
-	/*
-	 * public static String[] getGuidArray(String script) { String guids =
-	 * send(ReturnedType.arrayType + "##" + script);
-	 * 
-	 * // 如果返回的guids为空，则代表数组没有取得任何元素，返回一个空数组 if (guids.isEmpty()) { return new
-	 * String[0]; } return guids.split("#"); }
-	 */
 
 	public static String getJSONArray(String script) throws Exception {
 		String guids = send(ReturnedType.JSONArray + "##" + script);
@@ -105,7 +115,6 @@ public class MySocket {
 			System.out.println("The server listen to port: 5566");
 			server = new ServerSocket(5566);
 			// 创建一个ServerSocket在端口5566监听客户请求
-
 		} catch (Exception e) {
 			throw new Exception("can not listen to:" + e);
 		}
@@ -120,7 +129,26 @@ public class MySocket {
 		String request = null;
 		try {
 			Socket socket = null;
-			socket = server.accept();
+
+			final ExecutorService exec = Executors.newFixedThreadPool(1);
+			Callable<Socket> call = new Callable<Socket>() {
+				@Override
+				public Socket call() throws Exception {
+					return server.accept();
+				}
+			};
+			try {
+				Future<Socket> future = exec.submit(call);
+				// 超时时间 20s
+				socket = future.get(20 * 1000, TimeUnit.MILLISECONDS);
+			} catch (TimeoutException ex) {
+				String errorMsg = "被测应用无返回，socket 超时。";
+				System.err.println(errorMsg);
+				ex.printStackTrace();
+				exec.shutdown();
+				throw new Exception(errorMsg + ex);
+			}
+			exec.shutdown();
 
 			BufferedReader is = new BufferedReader(new InputStreamReader(
 					socket.getInputStream()));
@@ -143,15 +171,23 @@ public class MySocket {
 			os.print(script);
 			os.flush();
 
-			// 刷新输出流，使Client马上收到该字符串
-
 			os.close();
 			is.close();
 			socket.close();
 
 			// 第二次建立socket，获取 上一步运行的结果
-
-			socket = server.accept();
+			try {
+				Future<Socket> future = exec.submit(call);
+				// 超时时间 20s
+				socket = future.get(20 * 1000, TimeUnit.MILLISECONDS);
+			} catch (TimeoutException ex) {
+				String errorMsg = "被测应用无返回，socket 超时。";
+				System.err.println(errorMsg);
+				ex.printStackTrace();
+				exec.shutdown();
+				throw new Exception(errorMsg + ex);
+			}
+			exec.shutdown();
 
 			is = new BufferedReader(new InputStreamReader(
 					socket.getInputStream()));
