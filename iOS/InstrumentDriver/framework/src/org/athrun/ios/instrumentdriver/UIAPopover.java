d23a0bd34c1725e162936c74098a799ba2193d5a
yuanyao <yuanyao@d9bb56e2-10e4-4519-8022-6655a19093be>
2012-08-27 03:19:38 +0000

    元耀添加popover

---
+++ iOS/InstrumentDriver/framework/src/org/athrun/ios/instrumentdriver/UIAPopover.java
@@ -0,0 +1,24 @@
+package org.athrun.ios.instrumentdriver;
+
+/**
+ * 
+ * @author yuanyao
+ *
+ */
+public class UIAPopover extends UIAElement {
+	public UIAPopover() {
+		super();
+	}
+
+	public UIAPopover(String guid) {
+		super(guid);
+	}
+
+	public UIAActionSheet actionSheet() throws Exception {
+		return UIAElementHelp.getActionSheet(this.guid + ".actionSheet()");
+	}
+
+	public void dismiss() throws Exception {
+		MySocket.getVoid(this.guid + ".dismiss()");
+	}
+}
