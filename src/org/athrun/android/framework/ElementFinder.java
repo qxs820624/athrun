4543e04d1dc18264ec0ccb15b8e806cf3fd46185
deunamis <deunamis@d9bb56e2-10e4-4519-8022-6655a19093be>
2013-03-20 11:49:29 +0000

    remove view.hasWindowFocus to get popup menu

--- src/org/athrun/android/framework/ElementFinder.java
+++ src/org/athrun/android/framework/ElementFinder.java
@@ -108,8 +108,8 @@ public class ElementFinder {
 		if (1 == matchedCounts) {
 			logger.info("There is 1 matched view.");
 			View view = matches.get(0);
-			// add view.hasWindowFocus() to ensure the view is on the top layer
-			if (view.hasWindowFocus() && view.getVisibility() == View.VISIBLE && view.isShown()) {
+//			if (view.hasWindowFocus() && view.getVisibility() == View.VISIBLE && view.isShown()) {
+			if (view.getVisibility() == View.VISIBLE && view.isShown()) {
 				suitableViews = matches;
 			}
 			
